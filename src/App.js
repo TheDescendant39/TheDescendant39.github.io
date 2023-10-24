12e1f3489165d4ebfb5c72dbc871239461aed64f
Vansh Patel <vanshpatel@Utsavs-MacBook-Air.local>
2023-10-24 15:42:48 -0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vansh Patel's website is still under construction
         </a>
       </header>
     </div>
